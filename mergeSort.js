30d3ffd1d5cbaf5467b7f9f63e77f8eea9b27930
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-11 09:00:47 -0600

    Write while loop of helper merge function

--- mergeSort.js
+++ mergeSort.js
@@ -13,6 +13,23 @@ function mergeSort(array) {
     console.log(left);
     console.log(right);
     console.log(middle);
+    // Array slicing keeps going recursively until each integer is in its own array
+
+    function merge(leftArray, rightArray) { // Arrays have been deconstructed and passed into merge
+        let result = []; // For final merge
+        let i = 0; // These two are for the two different arrays to move forward
+        let j = 0;
+
+        while (i < leftArray.length && j < rightArray.length) { // While there are items in an array
+            if (leftArray[i] < rightArray[j]) { // If the integer in the left array is smaller than the right
+                result.push(leftArray[i]); // Add the integer into the result array
+                i++; // Advance left array
+            } else {
+                result.push(rightArray[j]); // Otherwise, add the integer from the right array
+                j++; // Advance right array
+            }
+        }
+    }
 }
 
 console.log(mergeSort(testArray));
